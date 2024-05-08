9829c30fe8166762f5b49b9ba6ad03a6ee611957
susovanpatra00 <susovanpatra00@gmail.com>
2024-05-08 10:58:44 +0530

    Requirements Installed

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Email-Spam-Detector-Using-ML"
+AUTHOR_USER_NAME = "susovanpatra00"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "susovanpatra00@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+
+
+"""
+This setup.py will remain same for every project,,just the REPO_NAME, AUTHOR_USER_NAME, SRC_REPO &
+AUTHOR_EMAIL will change.
+"""
